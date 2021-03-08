fa68591489efc2402e1b43dbf55eb7c62d6a2c78
rizalarisandy <79956954+rizalarisandy@users.noreply.github.com>
2021-03-08 10:24:26 +0700

    Add files via upload

---
+++ variabel.php
@@ -0,0 +1,10 @@
+<?php
+// kita akan membuat variable dalam php
+$namadepan ="rizal arisandy";
+$namatengah ="ari";
+$namabelakang ="sandy";
+$jumlahbarang = 4;
+$harga = 100000;
+
+echo "Nama saya adalah $namadepan $namatengah $namabelakang saya membeli buah naga sebanyak $jumlahbarang seharga $harga";
+?>
